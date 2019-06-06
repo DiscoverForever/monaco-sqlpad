a7809629537177e681ab4c7a665af6955560f99a
tangyy <>
2019-06-06 16:08:24 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,98 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div id="container" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import * as monaco from 'monaco-editor'
+import SQLSnippets from './snippets'
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: 'App',
+  props: {
+    value: {
+      type: String,
+      default: ''
+    }
+  },
+  data() {
+    return {
+      monacoEditor: null,
+      sqlSnippets: null
+    }
+  },
+  async mounted() {
+    this.initEditor()
+    const dbSchema = await this.getDbSchema()
+    this.sqlSnippets.setDbSchema(dbSchema)
+  },
+  methods: {
+    /**
+     * 初始化编辑器
+     */
+    initEditor() {
+      // 实例化snippets
+      this.sqlSnippets = new SQLSnippets(monaco, ['${ }'])
+      // 设置编辑器主题
+      monaco.editor.setTheme('vs-dark')
+      // 设置编辑器语言
+      monaco.languages.registerCompletionItemProvider('sql', {
+        triggerCharacters: [' ', '.', '$'],
+        provideCompletionItems: (model, position) =>
+          this.sqlSnippets.provideCompletionItems(model, position)
+      })
+      // 初始化编辑器
+      this.monacoEditor = monaco.editor.create(
+        document.getElementById('container'),
+        {
+          value: this.value,
+          language: 'sql',
+          minimap: {
+            enabled: true
+          },
+          suggestOnTriggerCharacters: true
+        }
+      )
+      // //监听变化
+      this.monacoEditor.onDidChangeModelContent(e => {
+        this.caretOffset = e.changes[0].rangeOffset // 获取光标位置
+        this.$emit('input', this.monacoEditor.getValue())
+      })
+    },
+    /**
+     * 获取数据库表及表字段
+     */
+    getDbSchema() {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve({
+            dbName: 'test',
+            tables: {
+              user: [
+                {
+                  name: 'username',
+                  type: 'varchar'
+                },
+                {
+                  name: 'password',
+                  type: 'varchar'
+                }
+              ]
+            }
+          })
+        }, 20000)
+      })
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html,
+body {
+  width: 100%;
+  height: 100%;
+}
+#container {
+  width: 100%;
+  height: 100%;
 }
 </style>
