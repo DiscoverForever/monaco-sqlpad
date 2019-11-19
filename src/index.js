639360a65431e868dcb06e33de3ae1bc54f8a8b9
tangyy <>
2019-11-19 17:33:39 +0800

    Mod 更新组件导出方式

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
-import MonacoSqlpad from './App'
-export default MonacoSqlpad
+import MonacoSqlpad from './App';
+
+/* istanbul ignore next */
+MonacoSqlpad.install = function(Vue) {
+  Vue.component(MonacoSqlpad.name, MonacoSqlpad);
+};
+
+export default MonacoSqlpad;
