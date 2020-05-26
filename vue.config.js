56e59b0ee742ab2de572fa5a7b4ebb31fe52991d
tangyy <tangyy@missfresh.cn>
2020-05-26 20:04:06 +0800

    Mod 尝试修复打包问题

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
 const path = require('path')
 console.log(path.resolve('src/index.js'))
 module.exports = {
@@ -9,9 +11,24 @@ module.exports = {
   },
   publicPath: './',
   configureWebpack: {
-    plugins: [new MonacoWebpackPlugin({
-      languages: ['sql'],
-      features: []
-    })]
+    plugins: [
+      new MonacoWebpackPlugin({
+        languages: ['sql'],
+        features: [],
+        publicPath: '/'
+      }),
+      new CopyWebpackPlugin(
+        {
+          patterns: [
+            {
+              context: 'node_modules/monaco-editor',
+              from: '*',
+              to: '[name].[ext]',
+              toType: 'template'
+            }
+          ]
+        }
+      )
+    ]
   }
 }
